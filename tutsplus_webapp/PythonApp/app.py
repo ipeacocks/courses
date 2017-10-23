7ffc7e07769b9fa2102251f92d97cda795881bd8
Yuriy Bychenok <ipeacocks@gmail.com>
2017-10-24 01:13:15 +0300

    tutsplus_webapp edited mysql creds

--- tutsplus_webapp/PythonApp/app.py
+++ tutsplus_webapp/PythonApp/app.py
@@ -6,8 +6,8 @@ mysql = MySQL()
 app = Flask(__name__)
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'jay'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'jay'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'qwerty123'
 app.config['MYSQL_DATABASE_DB'] = 'BucketList'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
