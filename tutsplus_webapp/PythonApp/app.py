1306ae65a29682c58e55c8f994b0dd5337e9668a
Yurii Bychenok <ipeacocks@gmail.com>
2017-10-23 14:59:52 +0300

    New webapp from tutsplus

---
+++ tutsplus_webapp/PythonApp/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+	return render_template('index.html')
+
+
+@app.route('/showSignUp')
+def showSignUp():
+	return render_template('signup.html')
+
+
+@app.route('/signUp', methods=['POST'])
+def signUp():
+	# read the posted values from the UI
+	_name = request.form['inputName']
+	_email = request.form['inputEmail']
+	_password = request.form['inputPassword']
+
+
+
+if __name__ == "__main__":
+	app.run()
