50059b1c9751d79b4f90dce370fb7b9d3f65128f
Bruce <ipeacocks@gmail.com>
2017-10-24 12:43:23 +0300

    Update spreadsheet.py

--- google_ssheet/spreadsheet.py
+++ google_ssheet/spreadsheet.py
@@ -7,9 +7,4 @@ creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', s
 client = gspread.authorize(creds)
 
 sheet = client.open("Legislators 2017 copy").sheet1
-
-# list_of_hashes = sheet.get_all_records()
-# list_of_hashes = sheet.row_values(2)
-# print(list_of_hashes)
-
 sheet.update_cell(2, 2, "I just wrote to a spreadsheet using Python!")
