1bd81179ff3d6ab04a31988abd98397d3240f628
Yuriy Bychenok <ipeacocks@gmail.com>
2017-11-04 12:40:05 +0200

    Some updates to django lessons

--- django-quick-start/hello_world_project/hello_world/urls.py
+++ django-quick-start/hello_world_project/hello_world/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from hello_world import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^better/$', views.better, name='better'),
 ]
