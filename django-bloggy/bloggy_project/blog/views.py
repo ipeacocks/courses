150f47e6ffd1bc828a3ae75c3a326e9395a7f699
Yuriy Bychenok <ipeacocks@gmail.com>
2017-11-15 00:40:04 +0200

    Added form for new post, other changes

--- django-bloggy/bloggy_project/blog/views.py
+++ django-bloggy/bloggy_project/blog/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
-
-# Create your views here
 from django.http import HttpResponse
-from django.template import Context, loader
+from django.template import Context, loader, RequestContext
+from django.shortcuts import get_object_or_404, render, redirect
 
 from blog.models import Post
+from blog.forms import PostForm
 
 
 # helper function
@@ -13,20 +11,21 @@ def encode_url(url):
     return url.replace(' ', '_')
 
 
-def popular_posts():
-    return Post.objects.order_by('-views')[:5]
+# def popular_posts():
+#     return Post.objects.order_by('-views')[:5]
 
 
 def index(request):
     latest_posts = Post.objects.all().order_by('-created_at')
+    popular_posts = Post.objects.order_by('-views')[:5]
     t = loader.get_template('blog/index.html')
     context_dict = {
         'latest_posts': latest_posts,
-        'popular_posts': popular_posts(),
+        'popular_posts': popular_posts,
     }
     for post in latest_posts:
         post.url = encode_url(post.title)
-    for popular_post in popular_posts():
+    for popular_post in popular_posts:
         popular_post.url = encode_url(popular_post.title)
     c = Context(context_dict)
     return HttpResponse(t.render(c))
@@ -37,12 +36,26 @@ def post(request, post_url):
     # increment the number of views and save it
     single_post.views += 1
     single_post.save()
+    popular_posts = Post.objects.order_by('-views')[:5]
     t = loader.get_template('blog/post.html')
     context_dict = {
         'single_post': single_post,
-        'popular_posts': popular_posts(),
+        'popular_posts': popular_posts,
     }
-    for popular_post in popular_posts():
+    for popular_post in popular_posts:
         popular_post.url = encode_url(popular_post.title)
     c = Context(context_dict)
     return HttpResponse(t.render(c))
+
+
+def add_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect(index)
+        else:
+            print(form.errors)
+    else:
+        form = PostForm()
+    return render(request, 'blog/add_post.html', {'form': form})
