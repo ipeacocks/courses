f28737f9fdc59f66e3fd5e3a88dbb2f0f6d2ce94
ipeacocks <ipeacocks@gmail.com>
2017-11-19 11:46:34 +0200

    added slug, cleaned code

--- django-bloggy/bloggy_project/blog/views.py
+++ django-bloggy/bloggy_project/blog/views.py
@@ -6,7 +6,11 @@ from blog.models import Post
 from blog.forms import PostForm
 
 
-# helper function
+
+####################
+# helper functions #
+####################
+
 def encode_url(url):
     return url.replace(' ', '_')
 
@@ -15,35 +19,31 @@ def get_popular_posts():
     return Post.objects.order_by('-views')[:5]
 
 
+##################
+# view functions #
+##################
+
 def index(request):
     latest_posts = Post.objects.all().order_by('-created_at')
-    popular_posts = get_popular_posts()
     t = loader.get_template('blog/index.html')
     context_dict = {
         'latest_posts': latest_posts,
-        'popular_posts': popular_posts,
+        'popular_posts': get_popular_posts(),
     }
-    for post in latest_posts:
-        post.url = encode_url(post.title)
-    for popular_post in popular_posts:
-        popular_post.url = encode_url(popular_post.title)
     c = Context(context_dict)
     return HttpResponse(t.render(c))
 
 
-def post(request, post_url):
-    single_post = get_object_or_404(Post, title=post_url.replace('_', ' '))
-    popular_posts = get_popular_posts()
+def post(request, slug):
+    single_post = get_object_or_404(Post, slug=slug)
     # increment the number of views and save it
     single_post.views += 1
     single_post.save()
     t = loader.get_template('blog/post.html')
     context_dict = {
         'single_post': single_post,
-        'popular_posts': popular_posts,
+        'popular_posts': get_popular_posts(),
     }
-    for popular_post in popular_posts:
-        popular_post.url = encode_url(popular_post.title)
     c = Context(context_dict)
     return HttpResponse(t.render(c))
 
@@ -53,9 +53,9 @@ def add_post(request):
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             form.save(commit=True)
-            title_value = form.cleaned_data['title']
-            redirect_link = ('/blog/{}/').format(encode_url(title_value))
-            return redirect(redirect_link)
+            # title_value = form.cleaned_data['title']
+            # redirect_link = ('/blog/{}/').format(encode_url(title_value))
+            return redirect(index)
         else:
             print(form.errors)
     else:
