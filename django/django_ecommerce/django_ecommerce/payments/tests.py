239fb91f970c8cbd0f47d5c2eb10fcb829f0cb28
Yuriy Bychenok <ipeacocks@gmail.com>
2017-12-04 01:29:37 +0200

    Tests

--- django/django_ecommerce/django_ecommerce/payments/tests.py
+++ django/django_ecommerce/django_ecommerce/payments/tests.py
@@ -1,16 +1,16 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from payments.models import User
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class UserModelTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.test_user = User(email="j@j.com", name='test user')
+        cls.test_user.save()
+
+    def test_user_to_string_print_email(self):
+        self.assertEquals(str(self.test_user), "j@j.com")
+
+    def test_get_by_id(self):
+        self.assertEquals(User.get_by_id(1), self.test_user)
