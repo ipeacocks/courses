46c17a37c0afbd4418fed01643a8c80b7f272a7c
Yuriy Bychenok <ipeacocks@gmail.com>
2017-12-04 00:25:45 +0200

    Tests

--- django/django_ecommerce/django_ecommerce/main/tests.py
+++ django/django_ecommerce/django_ecommerce/main/tests.py
@@ -1,67 +1,74 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 from django.core.urlresolvers import resolve
-from django.shortcuts import render_to_response
 from .views import index
-
+from django.shortcuts import render_to_response
+import mock
 from payments.models import User
 from django.test import RequestFactory
 
 
 class MainPageTests(TestCase):
 
+    ###############
+    #### Setup ####
+    ###############
+
+    @classmethod
+    def setUpClass(cls):
+        request_factory = RequestFactory()
+        cls.request = request_factory.get('/')
+        cls.request.session = {}
+
+    ##########################
+    ##### Testing routes #####
+    ##########################
+
     def test_root_resolves_to_main_view(self):
         main_page = resolve('/')
         self.assertEqual(main_page.func, index)
 
+    def test_returns_appropriate_html_response_code(self):
+        resp = index(self.request)
+        self.assertEquals(resp.status_code, 200)
 
-    def test_returns_appropriate_html(self):
-        index = self.client.get('/')
-        self.assertEquals(index.status_code, 200)
-
-
-    def test_uses_index_html_template(self):
-        index = self.client.get('/')
-        self.assertTemplateUsed(index, 'index.html')
-
+    #####################################
+    #### Testing templates and views ####
+    #####################################
 
     def test_returns_exact_html(self):
-        index = self.client.get("/")
+        resp = index(self.request)
         self.assertEquals(
-            index.content,
+            resp.content,
             render_to_response("index.html").content
         )
 
     def test_index_handles_logged_in_user(self):
-        # create the user needed for user lookup from index page
-        from payments.models import User
+        # Create the user needed for user lookup from index page
+        # Note that we are not saving to the database
         user = User(
             name='jj',
             email='j@j.com'
         )
-        user.save()
 
-        # create a Mock request object, so we can manipulate the session
-        request_factory = RequestFactory()
-        request = request_factory.get('/')
+        # Create a session that appears to have a logged in user
+        self.request.session = {"user": "1"}
 
-        # create a session that appears to have a logged in user
-        request.session = {"user": "1"}
+        with mock.patch('main.views.User') as user_mock:
 
-        # request the index page
-        resp = index(request)
+            # Tell the mock what to do when called
+            config = {'get.return_value': user}
+            user_mock.objects.configure_mock(**config)
 
+            # Run the test
+            resp = index(self.request)
 
-        # verify it returns the page for the logged in user
-        self.assertEquals(
-            resp.content,
-            render_to_response('user.html', {'user': user}).content
-        )
+            # Ensure we return the state of the session back to normal so
+            # we don't affest other tests
+            self.request.session = {}
 
-
+            # Verify it returns the page for the logged in user
+            expectedHtml = render_to_response('user.html', {'user': user}).content
+            self.assertEquals(
+                resp.content,
+                expectedHtml
+            )
