aa2eb60e8dc05bbddc6757494c53e8acf586deea
Yuriy Bychenok <ipeacocks@gmail.com>
2018-02-05 01:42:41 +0100

    Fields validation

--- flask/packt_flask/crimemap/crimemap.py
+++ flask/packt_flask/crimemap/crimemap.py
@@ -2,6 +2,11 @@ from dbhelper import DBHelper
 from flask import Flask, render_template, request
 import json
 
+import datetime
+import dateparser
+
+import string
+
 import dbconfig
 if dbconfig.test:
     from mockdbhelper import MockDBHelper as DBHelper
@@ -11,24 +16,45 @@ else:
 
 app = Flask(__name__)
 DB = DBHelper()
+categories = ['mugging', 'break-in']
+
+
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, "%Y-%m-%d")
+    except TypeError:
+        return None
+
+
+def sanitize_string(userinput):
+    whitelist = string.letters + string.digits + " !?$.,;:-'()&"
+    return filter(lambda x: x in whitelist, userinput)
+
 
 @app.route("/")
-def home():
+def home(error_message=None):
     crimes = DB.get_all_crimes()
-    print crimes
-    print '-----------------'
     crimes = json.dumps(crimes)
-    print crimes
-    return render_template("home.html", crimes=crimes)
+    return render_template("home.html", crimes=crimes,
+                            categories=categories,
+                            error_message=error_message)
 
 
 @app.route("/submitcrime", methods=['POST'])
 def submitcrime():
     category = request.form.get("category")
-    date = request.form.get("date")
-    latitude = request.form.get("latitude")
-    longitude = request.form.get("longitude")
-    description = request.form.get("description")
+    if category not in categories:
+        return home()
+    date = format_date(request.form.get("date"))
+    if not date:
+        return home("Invalid date. Please use yyyy-mm-dd format")
+    try:
+        latitude = request.form.get("latitude")
+        longitude = request.form.get("longitude")
+    except ValueError:
+        return home()
+    description = sanitize_string(request.form.get("description"))
     DB.add_crime(category, date, latitude, longitude, description)
     return home()
 
